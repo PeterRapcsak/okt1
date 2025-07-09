c3b97bde3865552610cff9afc81612e352eb4ff6
PeterRapcsak <72269551+PeterRapcsak@users.noreply.github.com>
2025-07-09 10:09:51 +0200

    xdsx
    cd

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 
-szo = input("Szó: ").lower()
 
-betuk = []
 
-for s in szo:
-    betuk.append(s)
 
-if ''.join(list(reversed(betuk))) == szo:
-    print("Palindrom")
-else:
-    print("nem az")
+
