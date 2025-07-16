993f45a67854890749ab9f94a525d8cab192a1d5
PeterRapcsak <72269551+PeterRapcsak@users.noreply.github.com>
2025-07-16 15:15:31 +0200

    xd
    xd

--- file1.py
+++ file1.py
@@ -1,4 +1,12 @@
+def maskEmail(email: str) -> str:
+    username = email.split("@")[0]
+    domain = email.split("@")[1]
 
+    first = username[0]
+    last = username[-1]
+    middle = "*" * (len(username) - 2)
 
-fact(3)
+    masked_username = first + middle + last
+    return masked_username + "@" + domain
 
+print(maskEmail("peter.rapcsak@gmail.com"))
