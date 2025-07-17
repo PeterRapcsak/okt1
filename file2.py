53f8425510cf85ef57aa4918a3690b025f1cbf44
PeterRapcsak <72269551+PeterRapcsak@users.noreply.github.com>
2025-07-17 11:53:55 +0200

    xd
    xd

--- file2.py
+++ file2.py
@@ -1,1 +1,22 @@
+#1. Caesar titkosító
+def caesarCipher(text: str, shift: int) -> str:
+    lista = []
+    for betu in "abcdefghijklmnopqrstuvwxyz":
+        lista.append(betu)
 
+    result = ""
+  
+    for i in text:
+        valami = lista.index(i)
+        ujvalami = (valami + shift) % 26
+        result += lista[ujvalami] #ujvalamiadik elemet hozzáadjuk a result stringhez
+
+    return result
+
+def main():
+  x = int(input("input a number: "))
+  titkositando = input("input the stuff to be encrypted: ")
+  titkositott = caesarCipher(titkositando, x)
+  return titkositott
+
+print(main())
